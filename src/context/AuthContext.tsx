98432888efc4276a6f68ce9f09f0fa5b8da47322
Vítor Marco <vitoradrianomarco@gmail.com>
2022-08-08 10:04:59 -0300

    Save user data

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,14 +1,22 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { api } from "../services/api";
+import NextRouter from "next/router";
 
 type SignInCredentials = {
   email: string;
   password: string;
 };
 
+type UserType = {
+  email: string;
+  permissions: Array<string>;
+  roles: Array<string>;
+};
+
 interface IAuthContextData {
   signIn(credentials: SignInCredentials): Promise<void>;
   isAuthenticated: boolean;
+  user?: UserType;
 }
 
 interface IAuthProviderProps {
@@ -18,15 +26,24 @@ interface IAuthProviderProps {
 export const AuthContext = createContext({} as IAuthContextData);
 
 export const AuthProvider = ({ children }: IAuthProviderProps) => {
-  const isAuthenticated = false;
+  const [user, setUser] = useState<UserType>();
+  const isAuthenticated = !!user;
 
   const signIn = async ({ email, password }: SignInCredentials) => {
     try {
-      const response = await api.post("sessions", {
+      const { data } = await api.post("sessions", {
         email,
         password,
       });
-      console.log(response);
+      const { toke, refreshToken, permissions, roles } = data;
+
+      setUser({
+        email,
+        permissions,
+        roles,
+      });
+
+      NextRouter.push("/dashboard");
     } catch (e) {
       console.log(e);
     }
@@ -37,6 +54,7 @@ export const AuthProvider = ({ children }: IAuthProviderProps) => {
       value={{
         signIn,
         isAuthenticated,
+        user,
       }}
     >
       {children}
---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,9 @@
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+
+const Dashboard = () => {
+  const { user } = useContext(AuthContext);
+  return <h1>Dashboard: {user?.email}</h1>;
+};
+
+export default Dashboard;
